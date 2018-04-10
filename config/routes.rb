bf0e1bd96a58459cd8b0a2533b8001c382e3c0f2
GabrielAfflitto <gafflitto92@gmail.com>
2018-04-10 16:19:30 -0600

    namespaces api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
     namespace :v1 do
-      resources :artists, only: [:index, :show]
+      resources :artists, only: [:index, :show] do
+        resources :albums, only: [:show]
+      end
+      resources :albums, only: [:index]
     end
   end
 end
