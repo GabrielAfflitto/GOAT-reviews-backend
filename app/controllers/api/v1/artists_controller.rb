e2bbdb82aa9cc7c8dd40267ee6d04f83d199a865
GabrielAfflitto <gafflitto92@gmail.com>
2018-04-10 12:35:24 -0600

    Added api serializer and controller for artists

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,7 +1,11 @@
-class Api::V1::Artists < ActionController::Base
+class Api::V1::ArtistsController < ActionController::Base
 
   def index
     render json: Artist.all
+  end
+
+  def show
+    render json: Artist.find(params[:id])
   end
 
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,4 @@ class Artist < ApplicationRecord
   has_many :songs
   has_many :albums
 
-  # iterate over array of artists in rails console and seed the database
-
 end
