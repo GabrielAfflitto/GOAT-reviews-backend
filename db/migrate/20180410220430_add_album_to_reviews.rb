c975bb8a55116353ef32cb257cd2c4cff76348af
GabrielAfflitto <gafflitto92@gmail.com>
2018-04-10 17:24:53 -0600

    migrations

--- db/migrate/20180410220430_add_album_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddAlbumToReviews < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :reviews, :album, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410220430) do
+ActiveRecord::Schema.define(version: 20180410190829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,15 +30,6 @@ ActiveRecord::Schema.define(version: 20180410220430) do
     t.string "name"
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.bigint "user_id"
-    t.string "body"
-    t.integer "rating"
-    t.bigint "album_id"
-    t.index ["album_id"], name: "index_reviews_on_album_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-
   create_table "songs", force: :cascade do |t|
     t.bigint "artist_id"
     t.string "name"
@@ -51,15 +42,6 @@ ActiveRecord::Schema.define(version: 20180410220430) do
     t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "spotify_url"
-    t.string "href"
-    t.string "uri"
-  end
-
   add_foreign_key "albums", "artists"
-  add_foreign_key "reviews", "albums"
-  add_foreign_key "reviews", "users"
   add_foreign_key "songs", "artists"
 end
