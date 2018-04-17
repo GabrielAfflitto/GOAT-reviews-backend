055ade9108dc5bdce37cdf237175344ffb4a3139
GabrielAfflitto <gafflitto92@gmail.com>
2018-04-17 16:56:23 -0600

    removed auth functionality on backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,4 @@
 class ApplicationController < ActionController::API
   include ActionController::Helpers
   include ActionController::Serialization
-
-  helper_method :current_user, :authenticate_user!
-
-  def current_user
-    @current_user ||= User.find(params[:id])
-  end
-
-  def logged_in?
-    current_user != nil
-  end
-
-  def authenticate_user!
-    head :unauthorized unless logged_in?
-  end
-
 end
