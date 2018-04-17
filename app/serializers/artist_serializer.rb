5bd115b7b5ae5cc2189e1fad04fdd7a3536c845b
GabrielAfflitto <gafflitto92@gmail.com>
2018-04-16 21:58:50 -0600

    created custom output for artist serializer

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,4 +1,15 @@
 class ArtistSerializer < ActiveModel::Serializer
   attributes :id, :name, :albums, :songs
 
+  def albums
+    object.albums.map do |album|
+      {id: album.id,
+      artist_id: album.artist_id,
+      name: album.name,
+      year: album.year,
+      spotify: album.spotify,
+      reviews: album.reviews}
+    end
+  end
+
 end
